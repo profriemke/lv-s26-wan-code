d53accaeb0be35ac1c6cb98ca22afe473fcb8059
prof_riemke <2960904+profriemke@users.noreply.github.com>
2026-04-28 15:46:25 +0200

    LV 28.04. (2)

--- 05-blog/blog/start/routes.ts
+++ 05-blog/blog/start/routes.ts
@@ -20,3 +20,43 @@ router.get('/', async ({ view })=>{
   return view.render('pages/home', { posts: posts})
 })
 
+// Dynamische Route für Post-Detailansicht
+
+router.get('/article/:id', async ({ view, params})=>{
+  const post = await db.from('posts')
+                       .select('*')
+                       .where({
+                          id: params.id
+                       })
+                       .first()
+
+  return view.render('pages/article', { post: post})
+})
+
+// Neue Posts
+
+router.get('/post/create', async ({ view })=>{
+  return view.render('pages/post_create')
+})
+
+router.post('/post/create', async ({ request, response })=>{
+  const title = request.input('title')
+  const teaser = request.input('teaser')
+  const text = request.input('text')
+
+  if(!title || !teaser || !text){
+    return response.redirect('/post/create')
+  }
+
+  const result = await db.table('posts')
+                         .insert(
+                          {
+                            title: title,
+                            teaser: teaser,
+                            text: text,
+                            author: 'Kalle',
+                            date: new Date().toDateString()
+                          })
+  console.log(result)
+  return response.redirect('/')
+})
