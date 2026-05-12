f04cac6ea8147b53b378f262f2cf92f8484328fc
prof_riemke <2960904+profriemke@users.noreply.github.com>
2026-05-12 15:24:51 +0200

    LV 12.05.

--- 05-blog/blog/start/routes.ts
+++ 05-blog/blog/start/routes.ts
@@ -11,53 +11,97 @@ import { middleware } from '#start/kernel'
 import { controllers } from '#generated/controllers'
 import router from '@adonisjs/core/services/router'
 import db from '@adonisjs/lucid/services/db'
+import hash from '@adonisjs/core/services/hash'
+
+router.get('/login', ({ view }) => {
+  return view.render('pages/login')
+})
+
+router.get('/logout', async ({ session, response })=>{
+  session.forget('login')
+  session.forget('firstname')
+  session.forget('lastname')
+  return response.redirect('/')
+})
 
 
-router.get('/', async ({ view })=>{
+router.post('/login', async ({ request, response, session }) => {
+  const login = request.input('login')
+  const password = request.input('password')
+
+  const result = await db.from('users')
+    .select('*')
+    .where({
+      login: login
+    })
+    .first()
+  if (!result) {
+    return response.redirect('/login')
+  }
+
+  if (await hash.verify(result.password, password) != true) {
+    return response.redirect('/login')
+  }
+
+  session.put('login', result.login)
+  session.put('firstname', result.firstname)
+  session.put('lastname', result.lastname)
+  return response.redirect('/')
+
+})
+
+
+router.get('/', async ({ view }) => {
   const posts = await db.from('posts')
-                        .select ('*')
+    .select('*')
 
- console.log(posts) 
-  return view.render('pages/home', { posts: posts})
+  console.log(posts)
+  return view.render('pages/home', { posts: posts })
 })
 
 // Dynamische Route für Post-Detailansicht
 
-router.get('/article/:id', async ({ view, params})=>{
+router.get('/article/:id', async ({ view, params }) => {
   const post = await db.from('posts')
-                       .select('*')
-                       .where({
-                          id: params.id
-                       })
-                       .first()
+    .select('*')
+    .where({
+      id: params.id
+    })
+    .first()
 
-  return view.render('pages/article', { post: post})
+  return view.render('pages/article', { post: post })
 })
 
 // Neue Posts
 
-router.get('/post/create', async ({ view })=>{
+router.get('/post/create', async ({ view, session, response }) => {
+  if (session.get('login') == undefined) {
+    return response.redirect('/login')
+  }
   return view.render('pages/post_create')
 })
 
-router.post('/post/create', async ({ request, response })=>{
+router.post('/post/create', async ({ request, response, session }) => {
+  if (session.get('login') == undefined) {
+    return response.redirect('/login')
+  }
   const title = request.input('title')
   const teaser = request.input('teaser')
   const text = request.input('text')
 
-  if(!title || !teaser || !text){
+  if (!title || !teaser || !text) {
     return response.redirect('/post/create')
   }
 
   const result = await db.table('posts')
-                         .insert(
-                          {
-                            title: title,
-                            teaser: teaser,
-                            text: text,
-                            author: 'Kalle',
-                            date: new Date().toDateString()
-                          })
+    .insert(
+      {
+        title: title,
+        teaser: teaser,
+        text: text,
+        author: session.get('login'),
+        date: new Date().toDateString()
+      })
   console.log(result)
   return response.redirect('/')
 })
@@ -78,12 +122,17 @@ router.get('/b', async ({ session }) => {
 
 router.get('/counter', async ({ view, session }) => {
   console.log(session.get('count'))
-  if(session.get('count') == undefined) {
-      // Sonderfall: count noch nicht vorhanden 
-    session.put('count', 1); 
-  }else{
-      // count erhöhen 
-    session.put('count', session.get('count')+1)
+  if (session.get('count') == undefined) {
+    // Sonderfall: count noch nicht vorhanden 
+    session.put('count', 1)
+  } else {
+    // count erhöhen 
+    session.put('count', session.get('count') + 1)
   }
-  return view.render('welcome',{count: session.get('count')})
+  return session.get('count')
+})
+
+router.get('/hash', async () => {
+  const hashedPassword = await hash.make("katze123");
+  return hashedPassword
 })
